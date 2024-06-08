8b264370a473ca47c74d8f627c39e68dfd8f7019
web design <wdesign589@gmail.com>
2024-06-08 21:48:09 +0400

    thirdccc cowqmmit

--- src/components/adminAdminBubble.jsx
+++ src/components/adminAdminBubble.jsx
@@ -19,7 +19,7 @@ function AdminAdminBubble({type, value}) {
     if(type == 'image'){
         return(
             <div className={`p-2 flex flex-row-reverse gap-2 space-x-2`}>
-                <div className='bg-notblack w-8 h-8 items-center p-1 rounded-full'>
+                <div className='bg-black w-8 h-8 items-center p-1 rounded-full'>
                     <img src={logo} alt="" className='w-6 h-6' />
                 </div>
                 <div className='bg-notblack  max-w-[70vw] rounded-xl p-2'>
